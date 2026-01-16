ff375d0153957250fed38e172b2d91aac2ab6f19
puskar2023 <puskarmandal04@gmail.com>
2026-01-16 23:06:33 +0530

    logical operator

---
+++ Basics/src/LogicalOperators.java
@@ -0,0 +1,8 @@
+public class LogicalOperators {
+    public static void main(String[] args) {
+        int p = 20, q = 10;
+        System.out.println(p > q && p >= q);
+        System.out.println(p < q || p > q);
+        System.out.println(p != q || p < q);
+    }
+}

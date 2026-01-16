b04167dda5e7ed066175734edf8cfd9b2576e898
puskar2023 <puskarmandal04@gmail.com>
2026-01-16 23:16:46 +0530

    bitwise operator

---
+++ Basics/src/BitwiseOperator.java
@@ -0,0 +1,9 @@
+public class BitwiseOperator {
+    public static void main(String[] args) {
+        int p = 9, q = 8;
+        System.out.println(p | q);
+        System.out.println(p & q);
+        System.out.println(p ^ q);
+        System.out.println(p << 1);
+    }
+}

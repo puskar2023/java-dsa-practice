ae94096f62b00f203453f95e317af85b888e0df8
puskar2023 <puskarmandal04@gmail.com>
2026-01-17 23:53:25 +0530

    total sum

--- Basics/src/Loops.java
+++ Basics/src/Loops.java
@@ -4,11 +4,21 @@ public class Loops {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int num = 1;
-        while(num < n){
-            System.out.println(num);
-            num++;
+
+        int sum = 0;
+        for(int i = 1; i <= n; i++){
+            sum += i;
         }
+        System.out.println(sum);
+
+
+
+//        while loop
+        // int num = 1;
+//        while(num < n){
+//            System.out.println(num);
+//            num++;
+//        }
 
 
     }

7966a957506042114dfb7eff90996e1827d907b9
puskar2023 <puskarmandal04@gmail.com>
2026-01-17 23:48:07 +0530

    while loop

---
+++ Basics/src/Loops.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Loops {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int num = 1;
+        while(num < n){
+            System.out.println(num);
+            num++;
+        }
+
+
+    }
+}

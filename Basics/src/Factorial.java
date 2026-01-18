149614fa1925fdbbbb5eabf0fd7d9650d6fb5c9a
puskar2023 <puskarmandal04@gmail.com>
2026-01-18 22:36:11 +0530

    first n factorial number

---
+++ Basics/src/Factorial.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 1; i <= n; i++){
+            int ans = 1;
+            for(int j = 1; j <= i; j++){
+                ans *= j;
+            }
+            System.out.println(ans);
+        }
+    }
+}

6e88f37ac882cc3ee0ce3999ad0537b3352cf01a
puskar2023 <puskarmandal04@gmail.com>
2026-01-19 23:41:33 +0530

    student class

--- OOPs/src/Main.java
+++ OOPs/src/Main.java
@@ -6,7 +6,15 @@ class Student {
 public class Main {
     public static void main(String[] args) {
         Student s1 = new Student();
+        s1.rollNo = 1;
+        s1.name = "Puskar";
         System.out.println(s1.rollNo);
         System.out.println(s1.name);
+
+        Student s2 = new Student();
+        s2.rollNo = 2;
+        s2.name = "Apurba";
+        System.out.println(s2.rollNo);
+        System.out.println(s2.name);
     }
 }

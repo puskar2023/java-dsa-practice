6aa75bc5be64acae973d46969a54fe8964858c2c
puskar2023 <puskarmandal04@gmail.com>
2026-01-19 23:34:29 +0530

    oops

---
+++ OOPs/src/Main.java
@@ -0,0 +1,12 @@
+class Student {
+    int rollNo;
+    String name;
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        System.out.println(s1.rollNo);
+        System.out.println(s1.name);
+    }
+}

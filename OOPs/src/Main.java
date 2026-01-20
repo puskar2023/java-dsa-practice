d2486047548965a00410f730da4bda962ccf0018
puskar2023 <puskarmandal04@gmail.com>
2026-01-20 22:26:26 +0530

    calculate sum

--- OOPs/src/Main.java
+++ OOPs/src/Main.java
@@ -1,20 +1,48 @@
-class Student {
-    int rollNo;
-    String name;
+import java.util.Scanner;
+
+class Algebra {
+    int sum(int a, int b){
+        int ans = a + b;
+        return ans;
+    }
 }
 
 public class Main {
     public static void main(String[] args) {
-        Student s1 = new Student();
-        s1.rollNo = 1;
-        s1.name = "Puskar";
-        System.out.println(s1.rollNo);
-        System.out.println(s1.name);
+        Algebra obj = new Algebra();
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("Sum of " + a + " and " + b + " is " + obj.sum(a, b));
 
-        Student s2 = new Student();
-        s2.rollNo = 2;
-        s2.name = "Apurba";
-        System.out.println(s2.rollNo);
-        System.out.println(s2.name);
     }
 }
+
+
+
+
+
+
+
+
+
+//class Student {
+//    int rollNo;
+//    String name;
+//}
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        Student s1 = new Student();
+//        s1.rollNo = 1;
+//        s1.name = "Puskar";
+//        System.out.println(s1.rollNo);
+//        System.out.println(s1.name);
+//
+//        Student s2 = new Student();
+//        s2.rollNo = 2;
+//        s2.name = "Apurba";
+//        System.out.println(s2.rollNo);
+//        System.out.println(s2.name);
+//    }
+//}

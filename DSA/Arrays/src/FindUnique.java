030cffadaff5b155f4bb2a2012b84792a00ccf5a
puskar2023 <puskarmandal04@gmail.com>
2026-01-24 22:55:43 +0530

    unique element

---
+++ DSA/Arrays/src/FindUnique.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class FindUnique {
+    static int findUnique(int[] arr){
+        int[] arr_2 = arr.clone();
+        int ans = -1;
+        for(int i = 0; i < arr_2.length; i++){
+            for(int j = i+1; j < arr_2.length; j++){
+                if(arr_2[i] == arr_2[j]){
+                    arr_2[i] = -1;
+                    arr_2[j] = -1;
+                }
+            }
+        }
+        for(int i = 0; i < arr_2.length; i++){
+            if(arr_2[i] > 0){
+                ans = arr_2[i];
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter all elements");
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Unique element: " + findUnique(arr));
+
+    }
+}

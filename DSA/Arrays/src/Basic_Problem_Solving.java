46b44cbe3d7d93c6eda2edb1108a6eda55eed947
puskar2023 <puskarmandal04@gmail.com>
2026-01-25 00:03:17 +0530

    reverse an array

--- DSA/Arrays/src/Basic_Problem_Solving.java
+++ DSA/Arrays/src/Basic_Problem_Solving.java
@@ -15,11 +15,35 @@ public class Basic_Problem_Solving {
         System.out.println("a" + a);
         System.out.println("b" + b);
     }
+    static int[] reverseArray(int[] arr){
+        int n = arr.length;
+        int i = 0;
+        int j = n-1;
+        while(i < j){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+        return arr;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        swap(a, b);
+//        int a = sc.nextInt();
+//        int b = sc.nextInt();
+//        swap(a, b);
+        System.out.println("Enter the size of the array");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter all elements");
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        int[] ans = reverseArray(arr);
+        for(int item: ans){
+            System.out.print(item + " ");
+        }
 
     }
 }

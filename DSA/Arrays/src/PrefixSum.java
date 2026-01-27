a65f2b92fddc77e92635d56ccad4fceaa2c6f666
puskar2023 <puskarmandal04@gmail.com>
2026-01-27 22:16:36 +0530

    is same sum subarray available

--- DSA/Arrays/src/PrefixSum.java
+++ DSA/Arrays/src/PrefixSum.java
@@ -17,6 +17,24 @@ public class PrefixSum {
         System.out.println(sum);
     }
 
+    static boolean isSubarrayAvailable(int[] arr){
+        int n = arr.length;
+        boolean check = false;
+        int sum = 0;
+        for(int i = 0; i < n; i++){
+            sum += arr[i];
+        }
+        int prefixsum = 0;
+        for(int i = 0; i < n-1; i++){
+            prefixsum += arr[i];
+            if(prefixsum == sum-prefixsum){
+                check = true;
+                break;
+            }
+        }
+        return check;
+    }
+
     static void display(int[] arr){
         for(int i: arr){
             System.out.print(i + " ");
@@ -28,23 +46,24 @@ public class PrefixSum {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of the array");
         int n = sc.nextInt();
-        int[] arr = new int[n + 1];
+        int[] arr = new int[n];
         System.out.println("Enter all elements");
-        for(int i = 1; i <= n; i++) {
+        for(int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
 //        int[] ans = prefixSum(arr);
 //        display(ans);
 
-        System.out.println("Enter number of querys");
-        int q = sc.nextInt();
-        while(q > 0){
-            System.out.println("Enter the range: ");
-            int r = sc.nextInt();
-            int l = sc.nextInt();
-            sumValues(arr, r, l);
-            System.out.println();
-            q--;
-        }
+//        System.out.println("Enter number of querys");
+//        int q = sc.nextInt();
+//        while(q > 0){
+//            System.out.println("Enter the range: ");
+//            int r = sc.nextInt();
+//            int l = sc.nextInt();
+//            sumValues(arr, r, l);
+//            System.out.println();
+//            q--;
+//        }
+        System.out.println(isSubarrayAvailable(arr));
     }
 }

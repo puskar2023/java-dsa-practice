b7443d34a3abab338578c50c5a044d0a7bf210ea
puskar2023 <puskarmandal04@gmail.com>
2026-01-29 23:31:41 +0530

    multiplication in 2d array

--- DSA/Arrays/src/Addition_2D_array.java
+++ DSA/Arrays/src/Addition_2D_array.java
@@ -21,28 +21,49 @@ public class Addition_2D_array {
         }
         return ans;
     }
+
+    static int[][] multiplication(int[][] arr1, int[][] arr2, int r1, int r2, int c1, int c2){
+        int[][] ans = new int[r1][c2];
+        if(c1 != r2){
+            System.out.println("multiplication is not possible");
+            return ans;
+        }
+
+        for(int i = 0; i < r1; i++){
+            for(int j = 0; j < c2; j++){
+                for(int k = 0; k < r2; k++){
+                  ans[i][j] += arr1[i][k]*arr2[k][j];
+                }
+            }
+        }
+        return ans;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter number of rows: ");
-        int r = sc.nextInt();
-        System.out.println("Enter number of columns: ");
-        int c = sc.nextInt();
-        int[][] array1 = new int[r][c];
+        System.out.println("Enter number of rows of first array: ");
+        int r1 = sc.nextInt();
+        System.out.println("Enter number of columns of first array: ");
+        int c1 = sc.nextInt();
+        int[][] array1 = new int[r1][c1];
         System.out.println("Enter all elements of first array: ");
-        for(int i = 0; i < r; i++){
-            for(int j = 0; j < c; j++){
+        for(int i = 0; i < r1; i++){
+            for(int j = 0; j < c1; j++){
                 array1[i][j] = sc.nextInt();
             }
         }
-        int[][] array2 = new int[r][c];
+        System.out.println("Enter number of rows of second array: ");
+        int r2 = sc.nextInt();
+        System.out.println("Enter number of columns of second array: ");
+        int c2 = sc.nextInt();
+        int[][] array2 = new int[r2][c2];
         System.out.println("Enter all elements of second array: ");
-        for(int i = 0; i < r; i++){
-            for(int j = 0; j < c; j++){
+        for(int i = 0; i < r2; i++){
+            for(int j = 0; j < c2; j++){
                 array2[i][j] = sc.nextInt();
             }
         }
-        int[][] ans = add(array1, r, c, array2);
-        display(ans, r, c);
+        int[][] ans = multiplication(array1, array2, r1, r2, c1, c2);
+        display(ans, r1, c2);
 
     }
 }

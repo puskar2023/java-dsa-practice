2b370ddc886271630b5b2b7890157304212fa0ad
puskar2023 <puskarmandal04@gmail.com>
2026-01-29 23:49:48 +0530

    transpose of 2D array

--- DSA/Arrays/src/Addition_2D_array.java
+++ DSA/Arrays/src/Addition_2D_array.java
@@ -39,31 +39,52 @@ public class Addition_2D_array {
         display(ans, r1, c2);
         return;
     }
+
+    static void transpose(int[][] arr){
+        int[][] ans = new int[arr.length][arr[0].length];
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[i].length; j++){
+                ans[i][j] = arr[j][i];
+            }
+        }
+        display(ans, arr.length, arr[0].length);
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter number of rows of first array: ");
-        int r1 = sc.nextInt();
-        System.out.println("Enter number of columns of first array: ");
-        int c1 = sc.nextInt();
-        int[][] array1 = new int[r1][c1];
-        System.out.println("Enter all elements of first array: ");
-        for(int i = 0; i < r1; i++){
-            for(int j = 0; j < c1; j++){
-                array1[i][j] = sc.nextInt();
+//        System.out.println("Enter number of rows of first array: ");
+//        int r1 = sc.nextInt();
+//        System.out.println("Enter number of columns of first array: ");
+//        int c1 = sc.nextInt();
+//        int[][] array1 = new int[r1][c1];
+//        System.out.println("Enter all elements of first array: ");
+//        for(int i = 0; i < r1; i++){
+//            for(int j = 0; j < c1; j++){
+//                array1[i][j] = sc.nextInt();
+//            }
+//        }
+//        System.out.println("Enter number of rows of second array: ");
+//        int r2 = sc.nextInt();
+//        System.out.println("Enter number of columns of second array: ");
+//        int c2 = sc.nextInt();
+//        int[][] array2 = new int[r2][c2];
+//        System.out.println("Enter all elements of second array: ");
+//        for(int i = 0; i < r2; i++){
+//            for(int j = 0; j < c2; j++){
+//                array2[i][j] = sc.nextInt();
+//            }
+//        }
+//        multiplication(array1, array2, r1, r2, c1, c2);
+        System.out.println("Enter number of rows: ");
+        int r = sc.nextInt();
+        System.out.println("Enter number of columns: ");
+        int c = sc.nextInt();
+        int[][] array = new int[r][c];
+        System.out.println("Enter all elements of array: ");
+        for(int i = 0; i < r; i++){
+            for(int j = 0; j < c; j++){
+                array[i][j] = sc.nextInt();
             }
         }
-        System.out.println("Enter number of rows of second array: ");
-        int r2 = sc.nextInt();
-        System.out.println("Enter number of columns of second array: ");
-        int c2 = sc.nextInt();
-        int[][] array2 = new int[r2][c2];
-        System.out.println("Enter all elements of second array: ");
-        for(int i = 0; i < r2; i++){
-            for(int j = 0; j < c2; j++){
-                array2[i][j] = sc.nextInt();
-            }
-        }
-        multiplication(array1, array2, r1, r2, c1, c2);
-
+        transpose(array);
     }
 }

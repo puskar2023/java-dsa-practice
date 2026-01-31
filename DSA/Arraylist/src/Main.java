23994af4490146e52a457255187ad21a58093ca9
puskar2023 <puskarmandal04@gmail.com>
2026-01-31 23:39:00 +0530

    add element in arraylist

--- DSA/Arraylist/src/Main.java
+++ DSA/Arraylist/src/Main.java
@@ -1,8 +1,19 @@
+import java.util.ArrayList;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Integer i = Integer.valueOf(6);
-        System.out.println(i);
+//        Integer i = Integer.valueOf(6);
+//        System.out.println(i);
+        ArrayList<Integer> l1 = new ArrayList<>();
+
+        l1.add(6);
+        l1.add(7);
+        l1.add(8);
+        l1.add(9);
+        for(int i = 0; i < l1.size(); i++){
+            System.out.println(l1.get(i));
+        }
     }
 }

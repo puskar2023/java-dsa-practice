fd25d13d4f2f79a8d7e366604b18605e2236c4a4
puskar2023 <puskarmandal04@gmail.com>
2026-02-03 23:52:39 +0530

    print elements in reverse

--- DSA/Recursion/src/Main.java
+++ DSA/Recursion/src/Main.java
@@ -11,9 +11,19 @@ public class Main {
         printIncreasing(n-1);
         System.out.println(n);
     }
+
+    static void printDecreasing(int n){
+        if(n == 1){
+            System.out.println(n);
+            return;
+        }
+        System.out.println(n);
+        printDecreasing(n-1);
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        printIncreasing(n);
+//        printIncreasing(n);
+        printDecreasing(n);
     }
 }

25393ef6b4a0fe568c2e77c87b13848164edb621
puskar2023 <puskarmandal04@gmail.com>
2026-02-04 23:22:25 +0530

    factorial and fibonacci

--- DSA/Recursion/src/Main.java
+++ DSA/Recursion/src/Main.java
@@ -20,10 +20,29 @@ public class Main {
         System.out.println(n);
         printDecreasing(n-1);
     }
+
+    static int factorial(int n){
+        if(n == 1){
+            return n;
+        }
+        return factorial(n-1)*n;
+    }
+
+    static int fibonacci(int n){
+        if(n == 0 ||n == 1){
+            return n;
+        }
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 //        printIncreasing(n);
-        printDecreasing(n);
+//        printDecreasing(n);
+//        System.out.println(factorial(n));
+        for(int i = 1; i <= n; i++){
+            System.out.print(fibonacci(i) + " ");
+        }
     }
 }

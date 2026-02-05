8daa87f60ba353b98a148f109a652b320c655e40
puskar2023 <puskarmandal04@gmail.com>
2026-02-05 23:01:35 +0530

    String basics

---
+++ DSA/String/src/strings/stringsbasics.java
@@ -0,0 +1,26 @@
+package strings;
+
+import java.util.*;
+
+public class stringsbasics {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+//        String str = sc.nextLine();
+//        System.out.println(str);
+//
+//        System.out.println(str.length());
+//
+//        System.out.println(str.charAt(3));
+//
+//        System.out.println(str.indexOf('l'));
+//
+//        String st1 = sc.nextLine();
+//        String st2 = sc.nextLine();
+//        System.out.println(st1.compareTo(st2));
+
+//        String str = "Hello World";
+//        System.out.println(str.contains("He"));
+//        System.out.println(str.startsWith("Hello"));
+    }
+}

d87b99f2a39b5a8e20ff9a9a887e1e40515db44a
puskar2023 <puskarmandal04@gmail.com>
2026-02-07 23:02:30 +0530

    count number of palindrone

---
+++ DSA/String/src/strings/countPalindrome.java
@@ -0,0 +1,32 @@
+package strings;
+import java.util.*;
+
+public class countPalindrome {
+    static boolean isPalindrome(String str){
+        int i = 0;
+        int j = str.length()-1;
+        while(i < j){
+            if(str.charAt(i) != str.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder str = new StringBuilder(sc.nextLine());
+
+        int c = 0;
+        for(int i = 0; i < str.length(); i++){
+            for(int j = i+1; j <= str.length(); j++){
+                if(isPalindrome(str.substring(i,j))){
+                    c++;
+                }
+            }
+        }
+        System.out.println(c);
+    }
+}

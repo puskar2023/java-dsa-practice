d6032b421e2cf68828864082dc2659a60ae605ac
puskar2023 <puskarmandal04@gmail.com>
2026-02-08 23:26:33 +0530

    checking pass by reference

--- OOPs/src/StudentData.java
+++ OOPs/src/StudentData.java
@@ -4,11 +4,17 @@ public class StudentData {
         int age;
         int roll_no;
     }
+    static void change(Student s){
+        s.age = 22;
+    }
     public static void main(String[] args) {
         Student s1 = new Student();
         s1.name = "Puskar";
         s1.age = 21;
         s1.roll_no = 75;
+        System.out.println(s1.age);
+        change(s1);
+        System.out.println(s1.age);
         System.out.println(s1.name);
     }
 }

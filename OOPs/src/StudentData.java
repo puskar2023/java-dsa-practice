dfdd5171a38df0a1eec074f886cffae26d6e7743
puskar2023 <puskarmandal04@gmail.com>
2026-02-08 23:23:26 +0530

    oops

---
+++ OOPs/src/StudentData.java
@@ -0,0 +1,14 @@
+public class StudentData {
+    public static class Student{
+        String name;
+        int age;
+        int roll_no;
+    }
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.name = "Puskar";
+        s1.age = 21;
+        s1.roll_no = 75;
+        System.out.println(s1.name);
+    }
+}

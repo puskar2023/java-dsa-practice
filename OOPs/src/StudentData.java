41fb6ab6b3dc26d127cdc40df712a5553cc6bd66
puskar2023 <puskarmandal04@gmail.com>
2026-02-09 23:42:33 +0530

    getters and setters

--- OOPs/src/StudentData.java
+++ OOPs/src/StudentData.java
@@ -1,9 +1,19 @@
-public class StudentData {
-    public static class Student{
-        String name;
-        int age;
-        int roll_no;
+
+class Student{
+    public String name;
+    public int age;
+    private int roll_no;
+
+    public int getRoll_no(){
+        return roll_no;
     }
+    public void setRoll_no(int rno){
+        roll_no = rno;
+    }
+}
+
+public class StudentData {
+
     static void change(Student s){
         s.age = 22;
     }
@@ -11,10 +21,9 @@ public class StudentData {
         Student s1 = new Student();
         s1.name = "Puskar";
         s1.age = 21;
-        s1.roll_no = 75;
-        System.out.println(s1.age);
-        change(s1);
+        s1.setRoll_no(75);
         System.out.println(s1.age);
         System.out.println(s1.name);
+        System.out.println(s1.getRoll_no());
     }
 }
